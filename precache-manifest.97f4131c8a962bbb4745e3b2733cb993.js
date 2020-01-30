5316185925e18ac1ee62561a1640e4b90c25dc2e
Alice <alise-fox@inbox.ru>
2020-01-31 02:54:48 +0300

    Updates

---
+++ precache-manifest.97f4131c8a962bbb4745e3b2733cb993.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766c39d6dab962e2b44921780f1cbc42",
+    "url": "/react-events/index.html"
+  },
+  {
+    "revision": "632d122a53844f7682c3",
+    "url": "/react-events/static/css/2.f785af3a.chunk.css"
+  },
+  {
+    "revision": "b0fa9aa01b739c941855",
+    "url": "/react-events/static/css/main.6ba177b1.chunk.css"
+  },
+  {
+    "revision": "632d122a53844f7682c3",
+    "url": "/react-events/static/js/2.7b802fac.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-events/static/js/2.7b802fac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0fa9aa01b739c941855",
+    "url": "/react-events/static/js/main.1aebd4bc.chunk.js"
+  },
+  {
+    "revision": "9567cf26a400052cbf4c",
+    "url": "/react-events/static/js/runtime-main.73adbb71.js"
+  },
+  {
+    "revision": "e1a15b9d67463aa75caadcbc93a96f03",
+    "url": "/react-events/static/media/no-money.e1a15b9d.svg"
+  }
+]);
